b45b0ef5e447667108a3ccae293dd54771348286
Rajesh P <penurajeshitsd@gmail.com>
2024-06-14 01:47:45 +0000

    first

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Component} from 'react'
+import './App.css'
+
+class App extends Component{
+    constructor(){
+        super()
+        this.state = {}
+    }
+
+    render(){
+        return(
+            <div className="App">
+                <h1>Welcome to My Web App</h1>
+            </div>
+        )
+    }
+}
+
+export default App
